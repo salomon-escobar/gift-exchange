0aee227e77b060a1febcdef8691733131a3e59d6
salomon.escobar <salomon.escobar@company-corp.com>
2020-04-15 13:55:02 -0500

    fixed save conflict

--- src/components/ImageGallery/MyGallery.js
+++ src/components/ImageGallery/MyGallery.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 
-
 const images = [
   {
-    original:  'https://picsum.photos/id/1018/1000/300/',
-    thumbnail: 'https://picsum.photos/id/1018/1000/150/',
-  }
+    original: 'https://picsum.photos/id/1018/1000/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/150/',
+  },
 ];
 
 class MyGallery extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showThumbnails: false,
+      showFullscreenButton: false,
+      showGalleryFullscreenButton: false,
+      showPlayButton: false,
+      showGalleryPlayButton: false,
+      fullscreen: false,
+      showNav: false
+    }
+  }
   render() {
     return (
-      <ImageGallery items={images} />
-    );
+      <ImageGallery
+        items={images}
+        showFullscreenButton={this.state.showFullscreenButton}
+        showPlayButton={this.state.showPlayButton}
+        showThumbnails={this.state.showThumbnails}
+        showGalleryFullscreenButton={this.state.showGalleryFullscreenButton}
+        showNav={this.state.showNav}
+      />
+    )
   }
 }
 
